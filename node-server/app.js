3fda562af41b436a7bdc287f92572f61b5e106e3
pragyakar <pragyakar75@gmail.com>
2020-01-30 16:41:34 +0545

    :package: Added mongoose and mongoose setup

--- node-server/app.js
+++ node-server/app.js
@@ -1,10 +1,16 @@
 const express = require("express");
 const graphqlHTTP = require("express-graphql");
+const mongoose = require("mongoose");
 const schema = require("./schema/schema");
 
 const app = express();
 const port = 4444;
 const db_source = process.env.DB_AUTH_SOURCE;
+
+mongoose.connect(db_source, { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connection.once('open', () => {
+  console.log('Sucessfully connected to DB');
+});
 
 app.use('/graphql', graphqlHTTP({
   schema,
@@ -13,5 +19,4 @@ app.use('/graphql', graphqlHTTP({
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
-  console.log(db_source);
 });
