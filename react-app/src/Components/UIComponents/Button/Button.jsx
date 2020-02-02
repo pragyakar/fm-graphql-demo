e657ec2082f9e159310131888485af1a9fa36fff
pragyakar <pragyakar75@gmail.com>
2020-02-02 18:59:27 +0545

    :sparkles: Added button component

---
+++ react-app/src/Components/UIComponents/Button/Button.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Button = (props) => {
+
+  const { text, type } = props;
+
+  return (
+    <span className={`button ${ type ? type : 'dark' }`}>
+      {text}
+    </span>
+  );
+}
+
+export default Button;
