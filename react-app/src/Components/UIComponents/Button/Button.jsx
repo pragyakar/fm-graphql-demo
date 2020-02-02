b0216c884d2d7589dc22ae03c0a4d3dac07d0cbc
pragyakar <pragyakar75@gmail.com>
2020-02-02 19:11:32 +0545

    :sparkles: Added line component

--- react-app/src/Components/UIComponents/Button/Button.jsx
+++ react-app/src/Components/UIComponents/Button/Button.jsx
@@ -2,10 +2,13 @@ import React from 'react';
 
 const Button = (props) => {
 
-  const { text, type } = props;
+  const { text, type, onClick } = props;
 
   return (
-    <span className={`button ${ type ? type : 'dark' }`}>
+    <span 
+      className={`button ${ type ? type : 'dark' }`}
+      onClick={onClick}
+    >
       {text}
     </span>
   );
---
+++ react-app/src/Components/UIComponents/Line/Line.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Line = () => {
+  return (
+    <hr className="line" /> 
+  );
+}
+
+export default Line;
