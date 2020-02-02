397418c6a1c7801b7abde7933cd65b0e7baabcd2
pragyakar <pragyakar75@gmail.com>
2020-02-02 18:43:17 +0545

    :sparkles: Added active route check and classname

--- react-app/src/Components/UIComponents/Navbar/NavItem.jsx
+++ react-app/src/Components/UIComponents/Navbar/NavItem.jsx
@@ -1,14 +1,26 @@
 import React from 'react';
+import { useLocation } from "react-router-dom";
 import { Link } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
+const activeCheck = (route, path) => {
+  if (route === path) {
+    return 'active';
+  } else {
+    return '';
+  }
+}
 
 const NavItem = (props) => {
 
   const { label, icon, route } = props;
 
+  const { pathname } = useLocation();
+  const active = activeCheck(route, pathname);
+  
   return (
     <Link to={route}>
-      <div className="navbar-item">
+      <div className={`navbar-item ${active}`}>
         <span className="navbar-icon">
           <FontAwesomeIcon icon={icon} />
         </span>
