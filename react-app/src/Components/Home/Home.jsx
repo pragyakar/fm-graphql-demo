77c2723b78e5e8afb78e79db52531f3b8f0a951c
pragyakar <pragyakar75@gmail.com>
2020-02-03 21:43:17 +0545

    :sparkles: Added OwnerList and PetsList components

--- react-app/src/Components/Home/Home.jsx
+++ react-app/src/Components/Home/Home.jsx
@@ -1,29 +1,17 @@
 import React, { Fragment } from 'react';
 import Line from '../UIComponents/Line';
 import TitleBar from './TitleBar';
-import PetCard from '../UIComponents/Card/PetCard';
-import OwnerCard from '../UIComponents/Card/OwnerCard';
+import PetsList from './PetsList';
+import OwnerList from './OwnersList';
 
 const Home = () => {
   return (
     <Fragment>
       <TitleBar />
       <Line />
-      <PetCard 
-        id={'123'} 
-        name={'Doggo Doe'} 
-        owner={'Johanne Doe'} 
-        age={'1'}
-        image={''}
-      />
+      <PetsList />
       <Line />
-      <OwnerCard 
-        id={'123'} 
-        name={'Johanne Doe'} 
-        email={'johanne@email.com'} 
-        petCount={'2'}
-        image={''}
-      />
+      <OwnerList />
     </Fragment>
   );
 }
