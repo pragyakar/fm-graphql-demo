cb344c837b9d09166d8d6b4d2996e36b62696bc3
pragyakar <pragyakar75@gmail.com>
2020-02-11 20:14:51 +0545

    :fire: Moved Form to new component

--- react-app/src/Components/Pets/AddPet/AddPet.jsx
+++ react-app/src/Components/Pets/AddPet/AddPet.jsx
@@ -1,81 +1,18 @@
 import React from 'react';
-import { graphql } from 'react-apollo';
-import { flowRight as compose } from 'lodash';
-import { useForm } from "react-hook-form";
 
-import { getOwnersDropdownQuery } from '../../../queries/owners';  
-import { addPetMutation } from '../../../queries/pets';
-import addPetSchema from './addPetSchema';
+import TopBar from '../../UIComponents/TopBar';
+import Line from '../../UIComponents/Line';
+import AddPetForm from './AddPetForm';
 
-const AddPet = (props) => {
-
-  const { loading, profiles } = props.getOwners;
-  const { register, handleSubmit, errors } = useForm({
-    validationSchema: addPetSchema
-  });
-  
-  const onSubmit = data => {
-    props.addPet({
-      variables: {
-        name: data.name, 
-        animal: data.animal,
-        breed: data.breed,
-        age: data.age,
-        imageUrl: data.imageUrl,
-        ownerId: data.ownerId
-      }
-    });
-  }
+const AddPet = () => {
   
   return (
     <div>
-      { loading ? 'Loading' :
-        <form onSubmit={handleSubmit(onSubmit)}>
-          <div className="field">
-            <label>Pet Name</label>
-            <input type="text" name="name" ref={register}/>
-            {errors.name && <p>{errors.name.message}</p> }
-          </div>
-          <div className="field">
-            <label>Animal</label>
-            <input type="text" name="animal" ref={register}/>
-            {errors.animal && <p>{errors.animal.message}</p>}
-          </div>
-          <div className="field">
-            <label>Breed</label>
-            <input type="text" name="breed" ref={register}/>
-            {errors.breed && <p>{errors.breed.message}</p> }
-          </div>
-          <div className="field">
-            <label>Age</label>
-            <input type="number" name="age" ref={register}/>
-            {errors.age && <p>{errors.age.message}</p> }
-          </div>
-          <div className="field">
-            <label>Image Url</label>
-            <input type="text" name="imageUrl" ref={register}/>
-            {errors.imageUrl && <p>{errors.imageUrl.message}</p> }
-          </div>
-          <div className="field">
-            <label>Owner</label>
-            <select ref={register} name="ownerId">
-              <option value="">Select owner</option>
-              { profiles.map((owner) =>
-                <option value={owner.id} key={owner.id}>
-                  {owner.name}
-                </option>
-              )}
-            </select>
-              {errors.ownerId && <p>{errors.ownerId.message}</p> }
-          </div>
-          <button type="submit">Add</button>
-        </form>
-      }
+      <TopBar title={'Add New Pets'} />
+      <Line />
+      <AddPetForm />
     </div>
   );
 }
 
-export default compose(
-  graphql(getOwnersDropdownQuery, {name: "getOwners"}),
-  graphql(addPetMutation, {name: "addPet"})
-)(AddPet);
+export default AddPet;
--- react-app/src/Components/UIComponents/TopBar/TopBar.jsx
+++ react-app/src/Components/UIComponents/TopBar/TopBar.jsx
@@ -9,11 +9,13 @@ const TopBar = (props) => {
   return (
     <div className="page-top-bar">
       <p className="page-title">{title}</p>
+      { sendTo ? 
       <span className="page-button">
         <Link to={sendTo}>
           <Button text="add new" type="dark" />
         </Link>
       </span>
+      : "" }
     </div>
   )
 }
